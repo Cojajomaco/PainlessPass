e7526ec89a0e2b17ed19d4037ad27b06c94d5c13
Cojajomaco <143226996+Cojajomaco@users.noreply.github.com>
2024-10-25 20:48:29 -0500

    Changes to tests,models,djhelper

--- painlessapp/tests.py
+++ painlessapp/tests.py
@@ -12,10 +12,10 @@ class UserPasswordsTestCase(TestCase):
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get(username="testuser").username, "testuser")
         self.assertEqual(User.objects.get(username="testuser").id, new_user.id)
-        self.assertEqual(User.objects.get(username="testuser").password, "testpass")
+        self.assertNotEquals(User.objects.get(username="testuser").password, "testpass")
         self.assertEqual(User.objects.get(username="testuser").email, "testuser@mek-tech.net")
         # Default folder object for the user should be created.
-        self.assertTrue(Folder.objects.exists(name="None", user_id=new_user.id))
+        self.assertTrue(Folder.objects.exists(name="None", user=new_user))
 
     def test_create_password(self):
         new_user = instantiate_user("testuser", "testpass", "testuser@mek-tech.net")
@@ -23,16 +23,16 @@ class UserPasswordsTestCase(TestCase):
         UserPass.objects.create(name="Test Pass", username="testuser", password="testpass",
                                            uri="http://testurl.com", note="Test note",
                                            user_id=new_user)
-        self.assertTrue(UserPass.objects.exists(name="Test Pass"))
-        self.assertTrue(UserPass.objects.exists(username="testuser"))
-        self.assertTrue(UserPass.objects.exists(password="testpass"))
-        self.assertTrue(UserPass.objects.exists(uri="http://testurl.com"))
-        self.assertTrue(UserPass.objects.exists(note="Test note"))
-        self.assertEqual(UserPass.objects.get(name="Test Pass").user_id, new_user.id)
+        self.assertTrue(UserPass.objects.filter(name="Test Pass").exists())
+        self.assertTrue(UserPass.objects.filter(username="testuser").exists())
+        self.assertTrue(UserPass.objects.filter(password="testpass").exists())
+        self.assertTrue(UserPass.objects.filter(uri="http://testurl.com").exists())
+        self.assertTrue(UserPass.objects.filter(note="Test note").exists())
+        self.assertEquals(UserPass.objects.get(name="Test Pass").user_id, new_user)
 
     def test_create_folder(self):
         new_user = instantiate_user("testuser", "testpass", "testuser@mek-tech.net")
         # Create a folder
         Folder.objects.create(name="Test Folder", user_id=User.objects.get(username="testuser"))
         self.assertTrue(Folder.objects.exists(name="Test Folder"))
-        self.assertTrue(Folder.objects.exists(user_id=new_user))
+        self.assertTrue(Folder.objects.exists(user=new_user))

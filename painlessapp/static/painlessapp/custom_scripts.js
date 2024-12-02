2b71c22cf207b9ba46e57218ac57feb6b08358f3
Cojajomaco <143226996+Cojajomaco@users.noreply.github.com>
2024-12-02 15:39:10 -0600

    Changes to Javascript search... spent too long on this...

--- painlessapp/static/painlessapp/custom_scripts.js
+++ painlessapp/static/painlessapp/custom_scripts.js
@@ -1,3 +1,18 @@
+// Function that returns true if a value is in a row. Used to determine
+// further search filtering in later functions.
+function checkRowForValue(row, value) {
+  // Iterate through each cell in the row
+  if (value == null || value === "") {
+      return true
+  }
+  // Check if value matches
+  return $(row).find('td').filter(function() {
+    return ($(this).text().toLowerCase().indexOf(value) > -1);
+  }).length > 0;
+  return false; // Value not found in the row
+}
+
+
 // Confirm before deleting something; taken from https://stackoverflow.com/questions/37398416/django-delete-confirmation
 function confirmDelete(event) {
     if (!confirm("Are you sure you want to delete this item?\n\nThere is no way to restore.")) {
@@ -8,20 +23,49 @@ function confirmDelete(event) {
     }
 }
 
+// Confirmation for deletion buttons
 const confirmDeleteButtons = document.querySelectorAll(".confirm-delete");
 confirmDeleteButtons.forEach(button => {
   button.addEventListener("click", confirmDelete);
 });
 
+// Filtering table by Folders (dropdown menu) and Search bar
 $(document).ready(function() {
     if($('#folderChoice')) {
+        // Folder dropdown filter
         $("#folderChoice").change(function () {
-            var selectedValue = $(this).val();
+            let searchValue = $("#passSearch").val().toLowerCase();
+            if (searchValue === null)
+            {
+                searchValue = "";
+            }
+            let selectedValue = $(this).val();
 
+            // Table filter, and Search bar filter, if available
             $("#passTable tbody tr").each(function () {
-                var columnValue = $(this).find("td:nth-child(5)").text(); // Assuming the column to filter is the second one
+                let columnValue = $(this).find("td:nth-child(5)").text(); // Assuming the column to filter is the second one
+                if ((selectedValue === "" || columnValue === selectedValue) && (checkRowForValue($(this), searchValue))) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        });
 
-                if (selectedValue === "" || columnValue === selectedValue) {
+
+        // Filter with search changes instead
+        $("#passSearch").on("keyup", function () {
+            let searchValue = $("#passSearch").val().toLowerCase();
+            if (searchValue === null)
+            {
+                searchValue = "";
+            }
+            let selectedValue = $("#folderChoice").val();
+
+            // Table filter, and Search bar filter, if available
+            $("#passTable tbody tr").each(function () {
+                let columnValue = $(this).find("td:nth-child(5)").text(); // Assuming the column to filter is the second one
+                if ((selectedValue === "" || columnValue === selectedValue) && (checkRowForValue($(this), searchValue))) {
                     $(this).show();
                 } else {
                     $(this).hide();
